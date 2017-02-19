327cfb50660df66729f421ff998d9f184161715b
Pedro Trindade <pedrotrindade@Pedros-Mac-mini.local>
2017-02-19 14:59:35 +0000

    Stage: Displaying all Items

---
+++ catalog.php
@@ -0,0 +1,30 @@
+<?php 
+
+$pageTitle = "Full Catalog";
+$section = null;
+
+if(isset($_GET["cat"])){
+    if($_GET["cat"] == "books") {
+        $pageTitle = "Books";
+        $section = "books";
+    } elseif($_GET["cat"] == "movies") {
+        $pageTitle = "Movies";
+        $section = "movies";
+    } elseif($_GET["cat"] == "music") {
+        $pageTitle = "Music";
+        $section = "music";
+    }
+}
+
+
+include("includes/header.php"); ?>
+<div class="section catalog page">
+    <div class="wrapper">
+        <h1><?php echo $pageTitle; ?></h1>
+        <ul>
+            <?php foreach($catalog as $item) { echo "<li>" . $item . "</li>"; } ?>
+        </ul>
+    </div>
+</div>
+</div> <!-- end content -->
+<?php include("includes/footer.php"); ?>
