b679c1e1c544e752eeed36f58b7b0304022fb287
Pedro Trindade <pedromdat@gmail.com>
2017-02-21 19:14:47 +0000

    Finished Form HTML

--- suggest.php
+++ suggest.php
@@ -1,11 +1,34 @@
-<?php 
+<?php
 $pageTitle = "Suggest a Media Item";
 $section = "suggest";
 include("includes/header.php"); ?>
 
-<div class="section page">
-    <h1>Suggest a Media File</h1>    
-</div>
+    <div class="section page">
+        <div class="wrapper">
+            <h1>Suggest a Media File</h1>
+            <p>If you think something I'm missing, let me know! Complete the form to send me an email.</p>
 
-</div> <!-- end content -->
+            <form method="post">
+                <table>
+                    <tr>
+                        <th><label for="name">Name</label></th>
+                        <td><input type="text" name="name" id="name"></td>
+                    </tr>
+                    <tr>
+                        <th><label for="email">Name</label></th>
+                        <td><input type="text" name="email" id="email"></td>
+                    </tr>
+                    <tr>
+                        <th><label for="details">Suggest Item Details</label></th>
+                        <td><textarea name="details" id="details" placeholder="Insert Text Here"></textarea></td>
+                    </tr>
+                </table>
+                <input type="submit" value="Send">
+            </form>
+
+        </div>
+
+    </div>
+    </div> <!-- end content -->
+
 <?php include("includes/footer.php"); ?>
